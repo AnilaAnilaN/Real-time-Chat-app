c1773cf909eee9f10e68f5ff0a7dafc1c7333056
Anila Nawaz <anilanawaz531@gmail.com>
2025-06-15 18:33:55 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-   origin: process.env.NODE_ENV === "development" ? "http://localhost:5173" : "",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
