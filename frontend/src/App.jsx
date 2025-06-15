2d328d8ec4c3ba4e9614555ebb85428d6519803e
Anila Nawaz <anilanawaz531@gmail.com>
2025-06-15 08:04:47 +0500

    removed settings button from the navbar before authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,22 @@
 import Navbar from "./components/Navbar";
-
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
-
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
-
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
-
-  console.log({ onlineUsers });
+  const location = useLocation();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
-
-  console.log({ authUser });
 
   if (isCheckingAuth && !authUser)
     return (
@@ -31,9 +25,12 @@ const App = () => {
       </div>
     );
 
+  // Determine if the current route is the HomePage and user is authenticated
+  const isHomePage = location.pathname === "/" && authUser;
+
   return (
     <div>
-      <Navbar />
+      <Navbar isAuthenticated={isHomePage} />
 
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
@@ -47,4 +44,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
