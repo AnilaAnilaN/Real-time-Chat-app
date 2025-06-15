608cd3b8367fcb623f61bc6905cbcdf18e2695dc
Anila Nawaz <anilanawaz531@gmail.com>
2025-06-15 11:13:00 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,20 +6,21 @@ import cors from "cors";
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "development" ? "http://localhost:5173" : "https://your-vercel-url.com",
+   origin: process.env.NODE_ENV === "development" ? "http://localhost:5173" : "https://your-vercel-url.com",
     credentials: true,
   })
 );
@@ -29,11 +30,13 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-connectDB();
-
-export default app;
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
