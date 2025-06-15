bf7e679123d345497b600b06c2dc51a1f1feaccc
Anila Nawaz <anilanawaz531@gmail.com>
2025-06-15 08:05:20 +0500

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import { MessageSquare, Settings } from "lucide-react";
 
-const Navbar = () => {
+const Navbar = ({ isAuthenticated }) => {
   return (
     <header
-      className="bg-base-100 border-b border-base-300  bg-gradient-to-r from-teal-500 to-pink-500 fixed w-full top-0 z-40 
+      className="bg-base-100 border-b border-base-300 bg-gradient-to-r from-teal-500 to-pink-500 fixed w-full top-0 z-40 
       backdrop-blur-lg bg-base-100/80"
     >
       <div className="container mx-auto px-4 h-16">
@@ -12,20 +12,19 @@ const Navbar = () => {
           <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary " />
+                <MessageSquare className="w-5 h-5 text-primary" />
               </div>
               <h1 className="text-lg font-bold text-white">ChatMate</h1>
             </Link>
           </div>
 
           <div className="flex items-center gap-2">
-            <Link
-              to="/settings"
-              className="btn btn-sm gap-2 transition-colors"
-            >
-              <Settings className="w-4 h-4" />
-              <span className="hidden sm:inline">Settings</span>
-            </Link>
+            {isAuthenticated && (
+              <Link to="/settings" className="btn btn-sm gap-2 transition-colors">
+                <Settings className="w-4 h-4" />
+                <span className="hidden sm:inline">Settings</span>
+              </Link>
+            )}
           </div>
         </div>
       </div>
